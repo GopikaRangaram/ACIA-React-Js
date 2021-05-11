c47e8522d72e81e0914a1b0d8441e05b17178ea1
GopikaRangaram <83133736+GopikaRangaram@users.noreply.github.com>
2021-05-11 15:02:53 +0530

    Update organisation.js

--- ACIA - footer/components/organisation.js
+++ ACIA - footer/components/organisation.js
@@ -6,24 +6,24 @@ import { Container,  Image, Row, Col} from 'react-bootstrap';
 const Organisation = () => {
 
     return(
-    <Container fluid className="container my-3" >
+    <Container fluid className="container my-3 mx-2" >
         
                 <Row className='py-5'>
                   
-                  <Col lg={3} xs={6} md={6}>
+                  <Col className='px-5' lg={3} xs={12} md={6}>
                   <Image className="logoIpsum" src={require('../images/Mask Group 11.png').default} />
                   </Col>
                 
-                  <Col lg={3} xs={6} md={6}>
+                  <Col className='px-5' lg={3} xs={12} md={6}>
                   <Image className="amara" src={require('../images/Mask Group 12.png').default} />  
                   </Col>
                            
-                  <Col lg={3} xs={6}  md={6}>
+                  <Col className='px-5' lg={3} xs={12}  md={6}>
                   <Image className="aven"  src={require('../images/Mask Group 13.png').default} />
                   </Col>
     
                                     
-                  <Col lg={3} xs={6}  md={6}>
+                  <Col className='px-5' lg={3} xs={12}  md={6}>
                   <Image className="treva"  src={require('../images/Mask Group 14.png').default} />
                   </Col> 
 
