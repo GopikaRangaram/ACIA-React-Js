ce19c07b34a080227601d0509bef652e5d1d4cea
GopikaRangaram <83133736+GopikaRangaram@users.noreply.github.com>
2021-05-11 19:06:07 +0530

    Update App.js

--- ACIA - home and motor/src/App.js
+++ ACIA - home and motor/src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import AboutPolicy from './components/dynamicForms/AboutPolicy';
+
 
 import HomeBody from './components/homeBody';
 import Footer from './components/footer';
@@ -11,16 +11,15 @@ import MotorInsuranceBody from './components/motorInsuranceBody'
 
 function App() {
   return (
-    // <Router>
-    //   <div>
-    //     <Switch>
-    //       <Route path="/home" component = {HomeBody} />
-    //       <Route path="/MotorInsurance" component = { MotorInsuranceBody} />
-    //     </Switch>
-    //   </div>
-    // <Footer /> 
-    // </Router>
-    <AboutPolicy />
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/home" component = {HomeBody} />
+          <Route path="/MotorInsurance" component = { MotorInsuranceBody} />
+        </Switch>
+      </div>
+    <Footer /> 
+    </Router>
   );
 }
 
