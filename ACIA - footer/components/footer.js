c8493e22030f49fba507a877daaf0bb13a49ea85
GopikaRangaram <83133736+GopikaRangaram@users.noreply.github.com>
2021-05-11 13:59:12 +0530

    Update footer.js

--- ACIA - footer/components/footer.js
+++ ACIA - footer/components/footer.js
@@ -9,16 +9,14 @@ const Footer = () => {
 
 <Container fluid className="footerContainer">
 
-<Row lg={5} md={3} xs={3} style={{marginLeft:"75px"}}>
+<Row fluid style={{marginLeft:"75px"}}>
    
-<Col style={{paddingTop:"115px"}}>
-       <Row className="aciaLogo" />
-       <Row className="description">ACIA is a complete insurance product solution that is pre packaged</Row>
-
+<Col lg={3} xs={4} md={4} style={{paddingTop:"115px"}}>
+       <Row fluid className="aciaLogo" />
+       <Row fluid className="description pr-5">ACIA is a complete insurance product solution that is pre packaged</Row>
 </Col>
 
 <Col>
-
       <p className="columnTitle"><b>Learn More</b></p>
       <ul className="list-unstyled pt-3">
         <li><a className="innerLink" href="/#">How it works?</a></li>
@@ -30,7 +28,6 @@ const Footer = () => {
 </Col>
 
 <Col>
-
       <p className="columnTitle"><b>Support</b></p>
       <ul className="list-unstyled pt-3">
         <li><a className="innerLink" href="/#">FAQ</a></li>
@@ -49,16 +46,16 @@ const Footer = () => {
       </ul>
 </Col>
 
-<Col lg={2}>
+<Col>
       <p className="columnTitle pb-3"><b>We're in Social</b></p>
-      <Row xs={1} md={1} lg={2}>
-       <Col className="p-0"><Image className="facebook" src={require('../images/Group 216.png').default} /></Col>
-       <Col className="p-0"><Image className="linkedIn" src={require('../images/Group 215.png').default} /></Col>
+      <Row fluid>
+       <Col xs={2} md={3} lg={4} className="p-0"><Image className="facebook" src={require('../images/Group 216.png').default} /></Col>
+       <Col xs={2} md={3} lg={4}  className="p-0 "><Image className="linkedIn" src={require('../images/Group 215.png').default} /></Col>
       </Row>
 
-      <Row xs={1} md={1} lg={2}  className='pt-3'>
-        <Col className="p-0"><Image className="twitter" src={require('../images/Group 217.png').default} /></Col>
-        <Col className="p-0"><Image className="youTube" src={require('../images/Group 218.png').default} /></Col>
+      <Row fluid className='pt-3'>
+        <Col xs={2} md={3} lg={4}  className="p-0"><Image className="twitter" src={require('../images/Group 217.png').default} /></Col>
+        <Col xs={2} md={3} lg={4}  className="p-0"><Image className="youTube" src={require('../images/Group 218.png').default} /></Col>
       </Row>
 
 </Col>
@@ -67,20 +64,18 @@ const Footer = () => {
 </Row>
 
 
-<Row lg={2} md={1} xs={1}  style={{marginLeft:"75px"}}>
+<Row fluid style={{marginLeft:"75px"}}>
 
-<Col className='py-3'>
+<Col lg={3} className='py-5'>
          <Row className="alignText"> US & CAN: 1 - 855 -758-0984 </Row>
          <Row className="alignText"> Worldwide: +1-424-781-3315 </Row>
 </Col>
 
 </Row>
 
-<Row lg={1} md={1} xs={1} className='py-2'>
- 
+<Row lg={1} className='py-3'>
 <hr className="horizontalLine" />
-<p className="bottomLine">All rights reserved. ACIA 2021</p>
-
+<p className="mt-2 pt-3 bottomLine">All rights reserved. ACIA 2021</p>
 </Row>
 
 </Container>
